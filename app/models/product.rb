d802bdfedeee3a447dcfe85aa1c40dcc0f0bee75
Preet191200 <pkachhia@rrc.ca>
2026-03-29 19:42:15 -0500

    Remove GitHub Actions workflow

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_one_attached :image
+
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
-has_one_attached :image
+
   def self.ransackable_attributes(auth_object = nil)
     ["id", "title", "description", "price", "stock_quantity", "category_id", "created_at", "updated_at"]
   end
