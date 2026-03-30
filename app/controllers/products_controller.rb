cd54c6316d7b68ec61c0743b4b288a961cfc94e4
Preet191200 <pkachhia@rrc.ca>
2026-03-29 20:53:18 -0500

    Add keyword search with category filter on products page (feature 2.6)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,19 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.includes(:category).all
+    @categories = Category.all
+    @products = Product.includes(:category)
+
+    if params[:query].present?
+      @products = @products.where(
+        "title ILIKE :q OR description ILIKE :q", q: "%#{params[:query]}%"
+      )
+    end
+
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    @products = @products.all
   end
 
   def show
