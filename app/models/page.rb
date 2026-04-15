6bde6ecf83d6f62be4c023a8e562b2b7af13da27
Preet191200 <pkachhia@rrc.ca>
2026-04-14 21:57:25 -0500

    Add Page model and ActiveAdmin registration for contact and about pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ApplicationRecord
+  validates :title, presence: true
+  validates :slug, presence: true, uniqueness: true
+  validates :content, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "title", "slug", "content", "created_at", "updated_at"]
+  end
 end
